c08d89d518dfea70eb93dac86e53fb4f19d8370f
Yer12 <gynatik3@gmail.com>
2022-02-10 00:09:54 +0600

    uploaded vue project

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <div id="nav">
+      <router-link to="/">Main</router-link> |
+      <router-link to="/about">My orders</router-link>|
+      <router-link to="/rakhmet">Rakhmet</router-link>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  position: absolute;
+  bottom: 0;
+  box-shadow: 0px -3px 3px rgba(0, 0, 0, 0.05);
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+#nav a {
+  text-decoration: none;
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Rakhmet.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="rakhmet">
+    <h1>This is an Rakhmet page</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Rakhmet"
+}
+</script>
+
+<style scoped>
+
+</style>
