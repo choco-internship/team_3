37254f01058c75890fcf30b204c9f26217bc8cb6
Yer12 <gynatik3@gmail.com>
2022-03-10 00:38:24 +0600

    deploying heroku

--- server.js
+++ server.js
@@ -1,35 +1,11 @@
-// const express = require('express')
-// const serveStatic = require('serve-static')
-// const path = require('path')
-//
-// const app = express()
-//
-// app.use('/', serveStatic(path.join(__dirname, '/dist')))
-// const port = process.env.PORT || 8080
-// app.listen(port)
-//
-// console.log("Listening on port: "+ port)
-
-const express = require('express');
-const history = require('connect-history-api-fallback')
-const path = require('path')
+const express = require('express')
 const serveStatic = require('serve-static')
-
+const path = require('path')
 
 const app = express()
 
-// Use a fallback for non-root routes (required for Vue router)
-//   NOTE: History fallback must be "used" before the static serving middleware!
-app.use(history({
-    // OPTIONAL: Includes more verbose logging
-    verbose: true
-}))
-
-// Serve static assets from the build files (images, etc)
-app.use(serveStatic(path.join(__dirname, '/dist')))
-
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
+app.listen(port)
 
-app.listen(port, () => {
-    console.log('Server started at http://localhost:8080')
-})
+console.log("Listening on port: "+ port)
