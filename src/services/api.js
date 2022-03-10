945065f077e829397473d78f8532ac99fa81fbf0
Yer12 <gynatik3@gmail.com>
2022-03-10 13:23:55 +0600

    fix(api): fixed baseURL on api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://142.93.107.238/api/",
+    baseURL: "https://intern.rahmetapp.kz/api",
 });
 
 api.interceptors.request.use(
